8a9c52576f700d0e61b23bc7fe3da111b50da62e
Shivansh Singh <singhshivansh12may@gmail.com>
2024-05-20 12:28:26 +0530

    Added image selecter

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,34 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import Button from '../components/Button'
-
+import * as ImagePicker from 'expo-image-picker';
 const Home = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+  const imageSource = selectedImage  ? { uri: selectedImage } : {uri:'https://docs.expo.dev/static/images/tutorial/background-image.png'};
   return (
     
     <View className=" bg-[#25292e] flex-1 items-center ">
       <View className=" flex-1 pt-[58px] w-full items-center">
-        <Image source={{uri:'https://docs.expo.dev/static/images/tutorial/background-image.png'}} className="h-[440] w-[80%] rounded-[18px]"></Image>
+     
+
+          <Image source={imageSource} className="h-[440] w-[80%] rounded-[18px]" />
+
       </View>
       <View style={styles.footerContainer}>
-        <Button label='Choose a photo' theme='primary'></Button>
+        <Button label='Choose a photo' theme='primary' onPress={pickImageAsync}></Button>
         <Button label='Use this photo'></Button>
       </View>
       <StatusBar style="auto" />
